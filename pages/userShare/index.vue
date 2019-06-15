9f8d9faa600fccd80416799bb2f26954ea8eb444
zehao.sun <zehao.sun@fengjr.com>
2019-06-16 00:02:39 +0800

    分享页面

--- pages/userShare/index.vue
+++ pages/userShare/index.vue
@@ -1,8 +1,31 @@
 <template>
+	<view>
+		<view class="uni-flex uni-column">
+			<view class="flex-item flex-item-V">
+				<view><button class="targetBtn" hover-class="none" @click="handleShare">让朋友也测测吧</button></view>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-</script>
 
-<style>
-</style>
+export default {
+	data() {
+		return {
+		};
+	},
+	//监听页面加载
+	onLoad: function(option) {
+		this.$store.commit('setCurrentPage', 'preparePage');
+	},
+	methods: {
+		handleShare: function() {
+			console.log("微信sdk接入")
+		}
+	},
+	components: {
+	}
+};
+</script>
+<style></style>
