077b32596f76e164219e7629b831b262678e9004
zehao.sun <zehao.sun@fengjr.com>
2019-08-09 16:47:01 +0800

    修改部分代码

--- pages/component/hasFollowPublic/hasFollowPublic.vue
+++ pages/component/hasFollowPublic/hasFollowPublic.vue
@@ -9,7 +9,17 @@
 			</view>
 			<view class="follow">
 				<!-- 公众号原始ID 3577957379 base64加密后:MzU3Nzk1NzM3OQ== -->
-				<a href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU3Nzk1NzM3OQ==#wechat_redirect">马上关注</a>
+				<!-- <a href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU3Nzk1NzM3OQ==#wechat_redirect">马上关注</a> -->
+				<a href="#" @click="handleFollow">马上关注</a>
+			</view>
+			<view v-show="showHelp">
+				<div class="masker" catchtouchmove="ture"></div>
+				<div class="showMsg">
+					<div class="qrcode">
+						<img :src="qrcode" />
+					</div>
+					<div class="closeMsg" @click="handleClose"></div>
+				</div>
 			</view>
 		</view>
 	</uni-nav-bar>
@@ -21,13 +31,21 @@
 		name: 'has-follow-public',
 		props: {},
 		data() {
-			return {};
+			return {
+				showHelp:false,
+				qrcode:'',
+			};
 		},
 		mounted: function() {},
 		methods: {
 			handleFollow: function() {
-				console.log(1);
-			}
+				const {origin} = window.location;
+				this.showHelp = true;
+				this.qrcode = `${origin}/build/static/image/qrCode.jpeg`;
+			},
+			handleClose: function() {
+				this.showHelp = false;
+			},
 		},
 		components: {
 			uniNavBar
@@ -75,5 +93,61 @@
 
 	.cu-item {
 		font-size: 18px;
+	}
+	
+	.masker {
+		pointer-events: none;
+		background-color: black;
+		left: 0;
+		opacity: 0.7;
+		position: fixed;
+		top: 0;
+		z-index: 9999;
+		height: 100%;
+		width: 100%;
+	}
+	
+	.showMsg {
+		position: fixed;
+		left: 0;
+		right: 0;
+		bottom: 25px;
+		top: 0;
+		margin: auto;
+		height: 60vh;
+		width: 87vw;
+		background-image: url(/build/static/image/common/showFollow.png);
+		background-repeat: no-repeat;
+		background-size: 100% 100%;
+		z-index: 9999;
+	}
+	
+	.showMsg .qrcode {
+		height: 20vh;
+		width: 34vw;
+		left: 0;
+		right: 0;
+		bottom: 3vh;
+		margin: auto;
+		position: absolute;
+	}
+	
+	.showMsg .qrcode img {
+		width: 100%;
+		height: 100%;
+	}
+	
+	.closeMsg {
+		position: absolute;
+		left: 0;
+		right: 0;
+		bottom: -10vh;
+		margin: auto;
+		height: 7vh;
+		width: 11vw;
+		background-image: url(/build/static/image/common/closeMsg2x.png);
+		background-repeat: no-repeat;
+		background-size: 100% 100%;
+		z-index: 9999;
 	}
 </style>
